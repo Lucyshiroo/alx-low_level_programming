c6fa7dd33f155cd94f18debc26404bf351f5aafa
Lucyshiroo <lucyshiroo54@gmail.com>
2023-07-31 19:03:39 -0700

    crackme

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  set_string - set value character to pointer
+ *  @s:pointer
+ *  @to: the character
+ *  Return: 0 always success
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
